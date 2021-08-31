9dfb915254124c8cdac348f1eb07e8c8ea15f3e5
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-31 12:37:06 -0400

    style to the center in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { TicTacToe } from './components/TicTacToe';
 
+const centerStyle = {
+    backgroundColor: "DarkSeaGreen",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minWidth: "100%",
+    minHeight: "98vh"
+}
+
 ReactDOM.render(
-    <TicTacToe />
+    <div style={ centerStyle }>
+        <TicTacToe />
+    </div>
     ,document.getElementById('root')
 );
 
