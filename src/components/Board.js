60cddb155f653686405a0190353c9f325c8f1dce
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-31 20:55:19 -0400

    refactor Board file

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Square } from './Square';
 
 export const Board = ({ squares, onClick }) => {
     return (
         <div className="game-board">
             { squares.map(( square, i ) => {
-                return <Square key= { i } value={ square } onClick= { () => onClick(i) } />
+                return (
+                    <button 
+                        className="box" 
+                        key={ i }
+                        onClick={ () => onClick(i) }>
+                            { square }
+                    </button>
+                )
             })}
         </div>
     )
