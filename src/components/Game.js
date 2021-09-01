7b37a4e34a851196f16196a18f3ee26474d18b5d
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-31 20:48:22 -0400

    clean code and finish

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,31 +4,27 @@ import { Board } from './Board';
 
 export const Game = () => {
 
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    console.log(history);
-    const [stepNumber, setStepNumber] = useState(0);
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(history[stepNumber]);
+    const [ squares, setSquares ] = useState( Array(9).fill(null) );
+    const [ xIsNext, setXIsNext ] = useState( true );
+    const winner = calculateWinner( squares );
     const xO = xIsNext ? "X" : "O";
 
     function handleClick(i) {
-        const historyPoint = history.slice(0, stepNumber + 1);
-        const current = historyPoint[stepNumber];
-        const squares = [ ...current ];
+        const _squares = [ ...squares ];
         // return if won or occupied
-        if (winner || squares[i]) return;
+        if (winner || _squares[i]) return;
         // select square
-        squares[i] = xO;
-        setHistory([ ...historyPoint, squares]);
-        setStepNumber(historyPoint.length);
+        _squares[i] = xO;
+        setSquares([ ..._squares ]);
         setXIsNext(!xIsNext);
     }
 
     return (
         <div className="container">
             <h1>TicTacToe in React.js</h1>
-            <h2>{ winner ? "Winner: " + winner : "Next Player: " + xO }</h2>
-            <Board squares={ history[stepNumber] } onClick={ handleClick } />
+            <h1>{ winner ? `Winner:  ${ winner }` : "Next Player: " + xO }</h1>
+            <button className="resetbtn" onClick={ () => { setSquares(Array(9).fill(null)) }}>Start Again</button>
+            <Board squares={ squares } onClick={ handleClick } />
         </div>
     )
 }
